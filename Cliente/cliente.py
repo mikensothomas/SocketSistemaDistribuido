5e5ab47bdc60e7ed7c69157d5e9acf014eea67ab
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-03-24 22:02:01 -0300

    Commit

--- Cliente/cliente.py
+++ Cliente/cliente.py
@@ -1,19 +1,22 @@
 import socket
 import os
 
-def cliente(filename, host, port):
+def cliente(filename, host, port, protocolo):
     BUFFER_SIZE = 4096
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print(f'Cliente {protocolo}://{host}:/{port}/{filename}')
         s.connect((host, port))
         with open(filename, 'rb') as f:
             while True:
                 data = f.read(BUFFER_SIZE)
                 if not data:
                     break
-                else:
-                    s.sendall(data)
-filename = 'arquivoCliente.exe'
+                s.sendall(data)
+                print("Arquivo enviado com sucesso!")
+
+FILENAME = 'arquivo1.html'
 HOST = '127.0.0.1'  
 PORT = 5000
-cliente(filename, HOST, PORT)
+PROTOCOLO = 'HTTP'
+cliente(FILENAME, HOST, PORT, PROTOCOLO)
--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -1,28 +1,29 @@
 import socket
 import os
 
-def servidor(file_name, host, port):
+def servidor(file, host, port, caminho):
     BUFFER_SIZE = 4096
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
-        print(f'O servidor está esperando a conexão do cliente no endreço IP: {host}: e na porta: {port}')
+        print(f'Servidor {host}/{port}/{caminho}')
         conn, addr = s.accept()
         with conn:
-            print(f'Conectado por {addr}')
-            if os.path.exists(file_name):
+            print(f'Conectado com o cliente por {addr}')
+            if os.path.exists(file):
                 print("Erro: Este arquivo já exixte.")
             else:
-                with open(file_name, 'wb') as f:
+                with open(file, 'wb') as f:
                     while True:
                         data = conn.recv(BUFFER_SIZE)
                         if not data:
                             break
                         f.write(data)
-                print('File received successfully!')
+                print('Arquivo recebido com sucesso!')
 
-saved_file_name = 'arquivoCliente.exe'
+SAVEFILE = 'arquivo1.html'
 HOST = '127.0.0.1'  
 PORT = 5000
-servidor(saved_file_name, HOST, PORT)
+CAMINHO = 'Área de trabalho/SocketSistemaDistribuido/Servidor'
+servidor(SAVEFILE, HOST, PORT, CAMINHO)
