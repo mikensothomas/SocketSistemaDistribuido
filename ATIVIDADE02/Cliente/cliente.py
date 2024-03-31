2b037a3099d010c133c771a2489a1a82bb221c9e
Mikenson Thomas <thomasmikenson40@gmail.com>
2024-03-31 02:27:09 -0300

    Commit

--- ATIVIDADE02/Cliente/cliente.py
+++ ATIVIDADE02/Cliente/cliente.py
@@ -1,40 +1,109 @@
 import socket
 
-def main():
-    # Configurando o cliente
-    host = '127.0.0.1'
-    port = 5000
+host = '127.0.0.1'
+porta = 8080
 
-    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    cliente.connect((host, port))
+def cliente(host, porta):
 
-    # Autenticando o aluno
-    matricula = input("Informe sua matrícula: ")
-    senha = input("Informe sua senha: ")
-    credenciais = matricula + ',' + senha  # Concatena matrícula e senha separados por vírgula
-    cliente.send(credenciais.encode())
+    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_cliente.settimeout(0.1)
 
-    resposta_servidor = cliente.recv(1024).decode()
+    endereco = (host, porta)
     
-    if resposta_servidor == "Autenticado":
-        print("Autenticado com sucesso!")
-        # Recebendo e respondendo as questões
-        while True:
-            enunciado = cliente.recv(1024).decode()
-            if enunciado == "Fim":
-                break
-            alternativas = (cliente.recv(1024).decode())
-            #print("OP")
-            print(enunciado)
-            print("Escolha uma das alternativas:", alternativas)
-            resposta = input().lower().strip()
-            cliente.send(resposta.encode())
-            resposta_servidor = cliente.recv(1024).decode()
-            print("Sua resposta foi:", resposta_servidor)
-            break
-    else:
-        print("Credenciais inválidas")
-    cliente.close()
+    socket_cliente.connect(endereco)
 
-if __name__ == "__main__":
-    main()
+    print("Conectado com o servidor")
+    print("")
+
+    mensagem1 = input("Digite sua matricula: ")
+    socket_cliente.send(mensagem1.encode())
+    mensagem2 = input("Digite sua senha: ")
+    socket_cliente.send(mensagem2.encode())
+    print("")
+    
+    try:
+        dado00 = socket_cliente.recv(1024).decode()
+        print(dado00)
+
+        dado11 = socket_cliente.recv(1024).decode()
+        print(dado11)
+        dado22 = socket_cliente.recv(1024).decode()
+        print(dado22)
+        dado33 = socket_cliente.recv(1024).decode()
+        print(dado33)
+        dado44 = socket_cliente.recv(1024).decode()
+        print(dado44)
+        dado55 = socket_cliente.recv(1024).decode()
+        print(dado55)
+    except socket.timeout:
+        mensagem3 = input("Resposta1: ")
+        socket_cliente.send(mensagem3.encode())
+
+    try:
+        print("")
+        dado66 = socket_cliente.recv(1024).decode()
+        print(dado66)
+        dado77 = socket_cliente.recv(1024).decode()
+        print(dado77)
+        dado88 = socket_cliente.recv(1024).decode()
+        print(dado88)
+        dado99 = socket_cliente.recv(1024).decode()
+        print(dado99)
+        dado10 = socket_cliente.recv(1024).decode()
+        print(dado10)
+    except socket.timeout:
+        mensagem4 = input("Resposta2: ")
+        socket_cliente.send(mensagem4.encode())
+
+    try:
+        print("")
+        dado3a = socket_cliente.recv(1024).decode()
+        print(dado3a)
+        dado3b = socket_cliente.recv(1024).decode()
+        print(dado3b)
+        dado3c = socket_cliente.recv(1024).decode()
+        print(dado3c)
+        dado3d = socket_cliente.recv(1024).decode()
+        print(dado3d)
+        dado3e = socket_cliente.recv(1024).decode()
+        print(dado3e)
+    except socket.timeout:
+        mensagem5 = input("Resposta3: ")
+        socket_cliente.send(mensagem5.encode())
+
+    try:
+        print("")
+        dado4a = socket_cliente.recv(1024).decode()
+        print(dado4a)
+        dado4b = socket_cliente.recv(1024).decode()
+        print(dado4b)
+        dado4c = socket_cliente.recv(1024).decode()
+        print(dado4c)
+        dado4d = socket_cliente.recv(1024).decode()
+        print(dado4d)
+        dado4e = socket_cliente.recv(1024).decode()
+        print(dado4e)
+    except socket.timeout:
+        mensagem6 = input("Resposta4: ")
+        socket_cliente.send(mensagem6.encode())
+
+    try:
+        print("")
+        dado4aa = socket_cliente.recv(1024).decode()
+        print(dado4aa)
+        dado4bb = socket_cliente.recv(1024).decode()
+        print(dado4bb)
+        dado4cc = socket_cliente.recv(1024).decode()
+        print(dado4cc)
+        dado4dd = socket_cliente.recv(1024).decode()
+        print(dado4dd)
+        dado4ee = socket_cliente.recv(1024).decode()
+        print(dado4ee)
+    except socket.timeout:
+        mensagem7 = input("Resposta5: ")
+        socket_cliente.send(mensagem7.encode())
+        print("")
+        resultado = socket_cliente.recv(1024).decode()
+        print(resultado)
+
+cliente(host, porta)
--- ATIVIDADE02/Servidor/servidor.py
+++ ATIVIDADE02/Servidor/servidor.py
@@ -1,63 +1,143 @@
 import socket
 
-# Dicionário com as credenciais dos alunos
-credenciais_alunos = {
-    "123456": "senha123",
-    "654321": "senha456"
-}
+host = '127.0.0.1'
+porta = 8080
 
-# Dicionário com as questões e respostas
-questoes = {
-    "1": {"enunciado": "Qual é a capital do Brasil?",
-          "alternativas": ["a) São Paulo", "b) Rio de Janeiro", "c) Brasília", "d) Belo Horizonte"],
-          "resposta": "c"},
-    "2": {"enunciado": "Quem escreveu 'Dom Casmurro'?",
-          "alternativas": ["a) Machado de Assis", "b) Jorge Amado", "c) Graciliano Ramos", "d) Lima Barreto"],
-          "resposta": "a"}
-}
+def servidor(host, porta):
 
-def main():
-    # Configurando o servidor
-    host = '127.0.0.1'
-    port = 5000
+    servidor_secket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    servidor.bind((host, port))
-    servidor.listen(1)
+    servidor_secket.bind((host, porta))
 
-    print("Servidor aguardando conexão...")
+    servidor_secket.listen()
 
-    conn, addr = servidor.accept()
-    print("Conexão estabelecida com:", addr)
+    print(f"Servidor está conectado no: {host}:{porta}")
+    print("Está esperando a conexão do cliente...")
 
-    # Autenticação do aluno
-    matricula, senha = conn.recv(1024).decode().split(',')
     
-    if matricula in credenciais_alunos and credenciais_alunos[matricula] == senha:
-        conn.send("Autenticado".encode())  # Envio da confirmação de autenticação
-        # Enviando questões para o aluno
-        try:
-            # Enviando questões para o aluno
-            for num, questao in questoes.items():
-                enunciado = questao["enunciado"]
-                print("Enviando enunciado:", enunciado)  # Verificação para debug
-                conn.send(enunciado.encode())
-                conn.send(str(questao["alternativas"]).encode())
-                conn.send("FimQuestao".encode())  # Envio da marcação de fim da questão
-                try:
-                    resposta = conn.recv(1024).decode().lower().strip()
-                except ConnectionResetError:
-                    print("Conexão fechada pelo cliente.")
-                    break
-                if resposta == questao["resposta"]:
-                    conn.send("Correta".encode())
-                else:
-                    conn.send("Incorreta".encode())
-        except Exception as e:
-            print("Erro:", e)
-        finally:
-            conn.send("Fim".encode())  # Envio da marcação de fim do envio de todas as questões
-        conn.close()
 
-if __name__ == "__main__":
-    main()
+    while True:
+        matricula = '12'
+        senha = '12'
+        questao_certa = 0
+        qtd_questao = 5
+        nota_total = 0
+
+        socket_cli, endereco_cli = servidor_secket.accept()
+        print("Conectado com um aluno")
+        print("")
+
+        dado_matri = socket_cli.recv(1024).decode()
+        print(f"Matrícula do aluno: {dado_matri}")
+        dado_senha = socket_cli.recv(1024).decode()
+        print(f"Senha do aluno: {dado_senha}")
+
+        print("")
+        if(dado_matri == matricula and dado_senha == senha):
+            mensagem0 = "Escolhe uma letra em cada questão:\n"
+            socket_cli.send(mensagem0.encode())
+
+            mensagem1 = "Capital do Brasil?\n"
+            a1 = "a) Santa Catarina\n"
+            b2 = "b) Rio de janeiro\n"
+            c3 = "c) Brasilia\n"
+            d4 = "d) Maranho\n"
+
+            socket_cli.send(mensagem1.encode())
+            socket_cli.send(a1.encode())
+            socket_cli.send(b2.encode())
+            socket_cli.send(c3.encode())
+            socket_cli.send(d4.encode())
+            dado1 = socket_cli.recv(1024).decode()
+            print(f"Resposta da primeira questão: {dado1}")
+
+            if(dado1 == 'c'):
+                questao_certa = questao_certa + 1
+                nota_total = nota_total + 2
+
+            print("")
+            mensagem3 = "2 + 3:\n"
+            a11 = "a) 4\n"
+            b22 = "b) 5\n"
+            c33 = "c) 7\n"
+            d44 = "d) 21\n"
+
+            socket_cli.sendall(mensagem3.encode())
+            socket_cli.send(a11.encode())
+            socket_cli.send(b22.encode())
+            socket_cli.send(c33.encode())
+            socket_cli.send(d44.encode())
+            dado2 = socket_cli.recv(1024).decode()
+            print(f"Resposta da segunda questão: {dado2}")
+
+            if(dado2 == 'b'):
+                questao_certa = questao_certa + 1
+                nota_total = nota_total + 2
+
+            print("")
+            mensagem4 = "10 / 2:\n"
+            a111 = "a) 5\n"
+            b222 = "b) 7\n"
+            c333 = "c) 32\n"
+            d444 = "d) 90\n"
+
+            socket_cli.send(mensagem4.encode())
+            socket_cli.send(a111.encode())
+            socket_cli.send(b222.encode())
+            socket_cli.send(c333.encode())
+            socket_cli.send(d444.encode())
+            dado3 = socket_cli.recv(1024).decode()
+            print(f"Resposta da terceira questão: {dado3}")
+
+            if(dado3 == 'a'):
+                questao_certa = questao_certa + 1
+                nota_total = nota_total + 2
+
+            print("")
+            mensagem5 = "9 * 9:\n"
+            a1111 = "a) 5\n"
+            b1111 = "b) 23\n"
+            c1111 = "c) 98\n"
+            d1111 = "d) 81\n"
+
+            socket_cli.send(mensagem5.encode())
+            socket_cli.send(a1111.encode())
+            socket_cli.send(b1111.encode())
+            socket_cli.send(c1111.encode())
+            socket_cli.send(d1111.encode())
+            dado4 = socket_cli.recv(1024).decode()
+            print(f"Resposta da quarta questão: {dado4}")
+
+            if(dado4 == 'd'):
+                questao_certa = questao_certa + 1
+                nota_total = nota_total + 2
+
+            print("")
+            mensagem6 = "7 * 7:\n"
+            a1111a = "a) 5\n"
+            b1111b = "b) 23\n"
+            c1111c = "c) 49\n"
+            d1111d = "d) 81\n"
+
+            socket_cli.send(mensagem6.encode())
+            socket_cli.send(a1111a.encode())
+            socket_cli.send(b1111b.encode())
+            socket_cli.send(c1111c.encode())
+            socket_cli.send(d1111d.encode())
+            dado5 = socket_cli.recv(1024).decode()
+            print(f"Resposta da quarta questão: {dado5}")
+
+            if(dado5 == 'c'):
+                questao_certa = questao_certa + 1
+                nota_total = nota_total + 2
+
+            nota = (f"Você respondeu {qtd_questao} questões \nAcertou {questao_certa} questões \nTirou {nota_total} pontos")
+            socket_cli.send(nota.encode())
+
+            if(nota):
+                break
+        else:
+            print("Senha ou matŕcula inválido")
+            break
+
+servidor(host, porta)
